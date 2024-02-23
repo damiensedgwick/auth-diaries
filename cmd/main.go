893537739e6f87e6b000307e1a545fa0dd31e9cc
Damien Sedgwick <damienksedgwick@gmail.com>
2024-02-23 12:19:07 +0000

    building a lovely little ui

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"io"
 
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -25,16 +29,57 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 func main() {
 	e := echo.New()
 
-	e.Use(middleware.Logger())
 	e.Static("/static", "static")
+	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.Secure())
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
 
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
+		sess, _ := session.Get("session", c)
+
+		if sess.Values["user"] != nil {
+			var user User
+
+			err := json.Unmarshal(sess.Values["user"].([]byte), &user)
+			if err != nil {
+				fmt.Println("error unmarshalling user value")
+			}
+
+			return c.Render(200, "index", newPageData(user))
+		}
+
 		return c.Render(200, "index", nil)
 	})
 
+	e.GET("/auth/sign-in", func(c echo.Context) error {
+		return c.Render(200, "auth-form", nil)
+	})
+
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+type PageData struct {
+	User User
+}
+
+func newPageData(user User) PageData {
+	return PageData{
+		User: user,
+	}
+}
+
+type User struct {
+	Name     string
+	Email    string
+	Password string
+}
+
+func newUser() User {
+	return User{
+		Name:  "Damien Sedgwick",
+		Email: "damienksedgwick@gmail.com",
+	}
 }
